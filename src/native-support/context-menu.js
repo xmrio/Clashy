3b35d553114b780bb71b9e4b251a156b59fee54b
SpongeNobody <>
2021-02-27 16:22:49 +0800

    respawn clash binary on unexpected exit. bump clash core version.

--- src/native-support/context-menu.js
+++ src/native-support/context-menu.js
@@ -1,8 +1,5 @@
 const contextMenu = require('electron-context-menu')
 
-// const { actionVisible, parseAction } = require('./actions')
-// const { ADD_SLIDE, DELETE_SLIDE } = require('../message-constants')
-
 let window
 
 contextMenu({
@@ -12,33 +9,7 @@ contextMenu({
         actions.cut(),
         actions.copy(),
         actions.paste()
-    ],
-	// append: (defaultActions, params, browserWindow) => [
-	// 	{
-    //         label: 'Insert new slide',
-    //         visible: actionVisible(params.linkURL),
-    //         click: () => {
-    //             const action = parseAction(params.linkURL)
-    //             if (window) {
-    //                 window.webContents.send(ADD_SLIDE, {
-    //                     id: action.id
-    //                 })
-    //             }
-    //         }
-    //     },
-    //     {
-    //         label: 'Delete',
-    //         visible: actionVisible(params.linkURL),
-    //         click: () => {
-    //             const action = parseAction(params.linkURL)
-    //             if (window) {
-    //                 window.webContents.send(DELETE_SLIDE, {
-    //                     id: action.id
-    //                 })
-    //             }
-    //         }
-	// 	}
-    // ]
+    ]
 });
 
 const injectContextMenu = win => window = win 
